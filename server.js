ee6e6e0938f7b00f1d389950c4abf9453b2f89cd
MANUCISNE <117774887+MANUCISNE@users.noreply.github.com>
2024-02-01 10:30:20 -0300

    update contact and email service

---
+++ server.js
@@ -0,0 +1,51 @@
+import nodemailer from "nodemailer";
+import express from "express";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const USER_EMAIL = process.env.EMAIL_USER;
+const USER_EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
+const PORT = process.env.PORT;
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => { 
+    res.send({ message: 'Hello World 2' });
+})
+
+// Rota para lidar com o formulário
+app.post('/enviar-email', (req, res) => {
+    const { nome, email, mensagem } = req.body;
+    
+  // Configuração do Nodemailer
+    const transporter = nodemailer.createTransport({
+        service: 'gmail', // Exemplo: 'Gmail'
+        auth: {
+            user: USER_EMAIL,
+            pass: USER_EMAIL_PASSWORD,
+        },
+    });
+
+  // Opções do email
+    const mailOptions = {
+        from: email,
+        to:  USER_EMAIL,// Pode ser o mesmo ou outro email   
+        subject: 'Novo Email do Formulário do Portfólio',
+        text: `Nome: ${nome}\nEmail: ${email}\nMensagem: ${mensagem}`,
+    };
+
+  // Enviar o email
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+        return res.status(500).send(error.toString());
+        }
+        res.status(200).send('Email enviado: ' + info.response);
+    });
+});
+
+    // Iniciar o servidor
+    app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+    });
--- src/widgets/ContactForm/ui/ContactForm.jsx
+++ src/widgets/ContactForm/ui/ContactForm.jsx
@@ -1,115 +1,117 @@
-import { useRef, useState, useEffect } from 'react'
-import emailjs from '@emailjs/browser'
-import styles from './ContactForm.module.css'
-import { Button } from '~/shared/UI/Button'
-import { useTranslation } from 'react-i18next'
+import { useRef, useState, useEffect } from "react";
+import axios from "axios";
+import styles from "./ContactForm.module.css";
+import { Button } from "~/shared/UI/Button";
+import { useTranslation } from "react-i18next";
 
 const ContactForm = ({ mode }) => {
-    const { t } = useTranslation()
-    const [formData, setFormData] = useState({
-        user_name: '',
-        user_email: '',
-        message: '',
-    })
+  const { t } = useTranslation();
+  const [formData, setFormData] = useState({
+    user_name: "",
+    user_email: "",
+    message: "",
+  });
+  const [formSent, setFormSent] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
 
-    const [formSent, setFormSent] = useState(false)
+  useEffect(() => {
+    const temp = localStorage.getItem("formData");
+    const loadedFormData = JSON.parse(temp);
 
-    useEffect(() => {
-        const temp = localStorage.getItem('formData')
-        const loadedFormData = JSON.parse(temp)
-
-        if (loadedFormData) {
-            setFormData(loadedFormData)
-        }
-    }, [])
-
-    useEffect(() => {
-        const temp = JSON.stringify(formData)
-        localStorage.setItem('formData', temp)
-    }, [formData])
-
-    const handleChange = (event) => {
-        setFormData({
-            ...formData,
-            [event.target.name]: event.target.value,
-        })
+    if (loadedFormData) {
+      setFormData(loadedFormData);
     }
+  }, []);
 
-    const form = useRef()
+  useEffect(() => {
+    const temp = JSON.stringify(formData);
+    localStorage.setItem("formData", temp);
+  }, [formData]);
 
-    const sendEmail = (e) => {
-        e.preventDefault()
-        emailjs
-            .sendForm(
-                'service_v7ftwkw',
-                'template_g0pejbk',
-                form.current,
-                'YWYXaVn5QRM0wuaKd'
-            )
-            .then(
-                (result) => {
-                    console.log(result.text)
-                },
-                (error) => {
-                    console.log(error.text)
-                }
-            )
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const form = useRef();
+
+  const sendEmail = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("/enviar-email", {
+        nome: formData.user_name,
+        email: formData.user_email,
+        mensagem: formData.message,
+      });
+
+      if (response.data.success) {
         setFormData({
-            user_name: '',
-            user_email: '',
-            message: '',
-        })
-        setFormSent(true)
+          user_name: "",
+          user_email: "",
+          message: "",
+        });
+        setFormSent(true);
+      } else {
+        setErrorMessage(t("email-not-sent"));
+      }
+
+      console.log(response.data.message);
+    } catch (error) {
+      console.error("Erro ao enviar e-mail:", error);
+      setErrorMessage(t("try-again-later"));
     }
+  };
 
-    return (
-        <div className={styles.contactFormContainer}>
-            <form
-                className={`${styles.contactForm} ${
-                    mode ? styles.contactFormDark : styles.contactFormLight
-                }`}
-                ref={form}
-                onSubmit={sendEmail}
-            >
-                <label>{t('name')}</label>
-                <input
-                    value={formData.user_name}
-                    onChange={handleChange}
-                    className={styles.contactFormInput}
-                    type="text"
-                    name="user_name"
-                    placeholder={t('name-placeholder')}
-                    required
-                />
-                <label>Email</label>
-                <input
-                    value={formData.user_email}
-                    onChange={handleChange}
-                    className={styles.contactFormInput}
-                    type="email"
-                    name="user_email"
-                    placeholder="YourName@email.com"
-                    required
-                />
-                <label>{t('message')}</label>
-                <textarea
-                    value={formData.message}
-                    onChange={handleChange}
-                    className={styles.contactFormArea}
-                    name="message"
-                    rows="7"
-                    placeholder={t('message-placeholder')}
-                    required
-                />
-                <Button type="submit" value="Send">
-                    {t('submit-btn')}
-                </Button>
-                {formSent && (
-                    <p className={styles.messageSent}>{t('thank-you')}</p>
-                )}
-            </form>
-        </div>
-    )
-}
+  return (
+    <div className={styles.contactFormContainer}>
+      <form
+        className={`${styles.contactForm} ${
+          mode ? styles.contactFormDark : styles.contactFormLight
+        }`}
+        ref={form}
+        onSubmit={sendEmail}
+      >
+        <label>{t("name")}</label>
+        <input
+          value={formData.user_name}
+          onChange={handleChange}
+          className={styles.contactFormInput}
+          type="text"
+          name="user_name"
+          placeholder={t("name-placeholder")}
+          required
+        />
+        <label>Email</label>
+        <input
+          value={formData.user_email}
+          onChange={handleChange}
+          className={styles.contactFormInput}
+          type="email"
+          name="user_email"
+          placeholder="YourName@email.com"
+          required
+        />
+        <label>{t("message")}</label>
+        <textarea
+          value={formData.message}
+          onChange={handleChange}
+          className={styles.contactFormArea}
+          name="message"
+          rows="7"
+          placeholder={t("message-placeholder")}
+          required
+        />
+        <Button type="submit" value="Send">
+          {t("submit-btn")}
+        </Button>
+        {formSent && <p className={styles.messageSent}>{t("thank-you")}</p>}
+        {errorMessage && <p className={styles.errorMessage}>{errorMessage}</p>}
+      </form>
+    </div>
+  );
+};
 
-export default ContactForm
+export default ContactForm;
